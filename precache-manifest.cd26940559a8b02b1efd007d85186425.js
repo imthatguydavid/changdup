a20690acfd328e8571eba92c22475c57a17832d3
David Kim <david.kim@kingsmen.cc>
2019-07-10 10:45:13 -0700

    Updates

---
+++ precache-manifest.cd26940559a8b02b1efd007d85186425.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747eb7daad4a2af4bf2a7dfc1f9f8da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9735dc016462196b2950",
+    "url": "/static/css/main.934a0327.chunk.css"
+  },
+  {
+    "revision": "52e32e41ebb84598283e",
+    "url": "/static/js/2.83d17e43.chunk.js"
+  },
+  {
+    "revision": "9735dc016462196b2950",
+    "url": "/static/js/main.9c6eb1ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a0041338892aee0d325322c69261b95",
+    "url": "/static/media/back.7a004133.svg"
+  },
+  {
+    "revision": "80f4caff0a6ed7152eaa2ca4e51766ad",
+    "url": "/static/media/bg2.80f4caff.png"
+  },
+  {
+    "revision": "48e77642c13ef8f9b9df7387b73fd4ee",
+    "url": "/static/media/bio.48e77642.png"
+  },
+  {
+    "revision": "783ce1095fc5b5f6a28137b6e8f13a98",
+    "url": "/static/media/bio2.783ce109.png"
+  },
+  {
+    "revision": "384b3523da310ff93ad1e4fe25b37a5e",
+    "url": "/static/media/header-img.384b3523.png"
+  },
+  {
+    "revision": "e85a1c1f873cc5c2dc3946dab0a6a55a",
+    "url": "/static/media/location.e85a1c1f.svg"
+  },
+  {
+    "revision": "6f98de6cde91fef51538f73947d053f0",
+    "url": "/static/media/next.6f98de6c.svg"
+  },
+  {
+    "revision": "47186f51c1f0bcb8161b00bd3622195f",
+    "url": "/static/media/time.47186f51.svg"
+  },
+  {
+    "revision": "cdc61a4c7de51ddd5d36ed90626d267b",
+    "url": "/static/media/wedding-details.cdc61a4c.png"
+  },
+  {
+    "revision": "58d24f81448e49d2030df9125452c46f",
+    "url": "/static/media/where-to-stay.58d24f81.png"
+  },
+  {
+    "revision": "7a38e93d3424226e956ff71f66c866bd",
+    "url": "/static/media/wishing-well.7a38e93d.png"
+  }
+]);
