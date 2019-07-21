7449e252211c27a524bb96c0ae7e5edde8c5da36
David Kim <david.kim@kingsmen.cc>
2019-07-21 16:51:02 -0700

    add sharon and david

--- src/components/WeddingParty/WeddingParty.js
+++ src/components/WeddingParty/WeddingParty.js
@@ -12,19 +12,20 @@ class WeddingParty extends React.Component {
             <h3 className={styles.title}>Groomsman</h3>
             <div className={styles.people}>
               <Party title='Best Man' name='Daniel Daegun Lee' image={require('../../static/daniel.png')} />
-              <Party title='Grooms Man' name='Russell Gabrillo' image={require('../../static/russ.png')} />
-              <Party title='Grooms Man' name='Mikhail Hosain' image={require('../../static/mikhail.png')} />
-              <Party title='Grooms Man' name='Andrew De La Merced' image={require('../../static/andrew.png')} />
-              <Party title='Grooms Man' name='David Kim' image={require('../../static/bio.png')} />
+              <Party title='Groomsman' name='Andrew De La Merced' image={require('../../static/andrew.png')} />
+              <Party title='Groomsman' name='Mikhail Hosain' image={require('../../static/mikhail.png')} />
+              <Party title='Groomsman' name='Russ Gabrillo' image={require('../../static/russ.png')} />
+              <Party title='Groomsman' name='David Kim' image={require('../../static/david.png')} />
             </div>
           </div>
           <div className={styles.group}>
             <h3 className={styles.title}>Bridal Party</h3>
             <div className={styles.people}>
               <Party title='Maid of Honor' name='Melissa De La Merced' image={require('../../static/melissa.png')} />
-              <Party title='Bridesmaid' name='Cheryl Directo' image={require('../../static/cheryl.png')} />
-              <Party title='Bridesmaid' name='Molly Anabeze' image={require('../../static/molly.jpg')} />
+              <Party title='Bridesmaid' name='Sharon Chang' image={require('../../static/sharon.png')} />
               <Party title='Bridesmaid' name='Suzy Chang' image={require('../../static/suzy.png')} />
+              <Party title='Bridesmaid' name='Molly Anabeza' image={require('../../static/molly.jpg')} />
+              <Party title='Bridesmaid' name='Cheryl Directo' image={require('../../static/cheryl.png')} />
             </div>
           </div>
         </div>
