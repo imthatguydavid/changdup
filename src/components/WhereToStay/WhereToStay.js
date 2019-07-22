48d9b127d2bee3aac71017e86e36b12bc94f3a01
David Kim <david.kim@kingsmen.cc>
2019-07-21 17:16:44 -0700

    small changes to where to stay

--- src/components/WhereToStay/WhereToStay.js
+++ src/components/WhereToStay/WhereToStay.js
@@ -9,8 +9,14 @@ class WhereToStay extends React.Component {
         <div className={styles.contentContainer}>
           <div className={styles.content}>
             <h2 className="pageTitle">Where To Stay</h2>
+            <strong>Residence Inn By Marriot Camarillo</strong>
+            <div className={styles.address}>
+              <p>2912 Petit Street</p>
+              <p>Camarillo, California 93012</p>
+            </div>
+
             <p>Rooms are ready to be reserved by calling reservations (800.321.2211) and referencing the <strong>Chang/Dela Wedding room block</strong>.</p>
-            <p>All rooms must be booked by 08/23/19</p>
+            <p>All rooms must be booked by <strong>08/23/19</strong></p>
             <div className={styles.buttonContainer}>
               <button className="cta"><a href="https://www.marriott.com/events/start.mi?id=1556313200120&key=GRP" target="_blank">BOOK NOW</a></button>
               <button className="cta"><a href="https://goo.gl/maps/DhCYg4ygnd5unHwJ7" target="_blank">GET DIRECTIONS</a></button>
